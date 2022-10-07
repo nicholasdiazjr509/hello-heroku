7e0c3f88eaa892efe91fd55706a8ea815e9a3f30
Nicholas Diaz Jr <nicholasdiazjr509@gmail.com>
2022-10-07 14:08:30 -0500

    initial

---
+++ src/test/java/com/example/helloheroku/HelloherokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloherokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
